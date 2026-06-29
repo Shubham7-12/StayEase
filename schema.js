a84e2a6b0145ca25a149901b636392a297811b9d
Shubham Kumar <kshubham957023@gmail.com>
2026-06-30 00:53:00 +0530

    Restructure routes using Express Router

--- schema.js
+++ schema.js
@@ -3,9 +3,9 @@ const Joi = require('joi');
 module.exports.listingSchema = Joi.object({
     listing : Joi.object({
         title: Joi.string().required(),
-        discription: Joi.string().required(),
+        description: Joi.string().required(),
         location: Joi.string().required(),
-        country: Joi.number().required(),
+        country: Joi.string().required(),
         price: Joi.number().required().min(1),
         image: Joi.string().allow("",null)
     }).required()
